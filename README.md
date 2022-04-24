60bad07650231e7489a76aaf564a9a71923c3b2c
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-04-24 19:53:55 -0300

    commit-detalhes-cadastro

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# Pokedex
+# POKEDEX
 
 ## Descrição
+Projeto realizado como forma de aprendizado no curso de FullStack na BlueEdTech . 2022 . Modulo 2.
 
-Projeto realizado com fins de aprendizado para o curso FUllStack da BlueEdTech - modulo 2-
+## Stacks
+As ferramentas usadas para este projeto são:
 
-## Stacks Usadas
+<div style="display: flex">
+    <img src="https://img.icons8.com/color/96/000000/javascript--v1.png"/>
+    <img src="https://img.icons8.com/ios-filled/100/000000/css3.png"/>
+    <img src="https://img.icons8.com/color/144/000000/html-5--v2.png"/>
+    <img src="https://img.icons8.com/color/144/000000/nodejs.png"/>
+</div>
 
-Ferramentas usadas no projeto:
+## Contato
+<a href="https://www.linkedin.com/in/enrique-azevedo-molina-306573140/" target="blank">
+    <img src="https://img.icons8.com/office/80/000000/linkedin.png"/>
+</a
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 const port = 3001;
 app.listen(port, () => {
   console.log(`Rodando na porta ${port}`);
@@ -13,54 +15,69 @@ app.listen(port, () => {
 
 let pokemons = [
   {
-    numero:1,
+    numero: 1,
     nome: "Bulbasaur",
     tipo: "Grass",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-    descricao:"There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-    altura:"0.7 m",
-    peso:"6.9 kg",
-    categoria:"Seed",
-    habilidade:"Overgrow",
-
+    descricao:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    altura: "0.7 m",
+    peso: "6.9 kg",
+    categoria: "Seed",
+    habilidade: "Overgrow",
   },
-  { 
-    numero:2,
+  {
+    numero: 2,
     nome: "Charmander",
     tipo: "Fire",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-    descricao:"It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-    altura:"0.6 m",
-    peso:"8.5 kg",
-    categoria:"Lizard",
-    habilidade:"Blaze",
+    descricao:
+      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    altura: "0.6 m",
+    peso: "8.5 kg",
+    categoria: "Lizard",
+    habilidade: "Blaze",
   },
   {
-    numero:3,
+    numero: 3,
     nome: "Squirtle ",
     tipo: "Water",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-    descricao:"When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-    altura:"0.5 m",
-    peso:"9.0 kg",
-    categoria:"Tiny Turtle",
-    habilidade:"Torrent",
+    descricao:
+      "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+    altura: "0.5 m",
+    peso: "9.0 kg",
+    categoria: "Tiny Turtle",
+    habilidade: "Torrent",
   },
-
 ];
 app.get("/", (req, res) => {
   res.render("index.ejs", {
     pokemons,
   });
-  
 });
 app.get("/cadastro", (req, res) => {
   res.render("cadastro.ejs", {
     pokemons,
   });
-  
 });
 
-app.get("/detalhes", (req, res) => {
-  res.send("Teste Detalhes");
+app.get("/detalhes/:id", (req, res) => {
+  let pokemon = [];
+  pokemons.filter((element) => {
+    if (element.numero === +req.params.id) {
+      pokemon.push(element);
+    }
+  });
+  res.render("detalhes.ejs", {
+    pokemon,
+  });
+});
+app.post("/cadastro", (req, res) => {
+  const pokemon = req.body;
+  console.log(pokemons.length);
+  pokemon.numero = pokemons.length + 1;
+  pokemons.push(pokemon);
+  console.log(pokemon);
+  res.redirect("/cadastro");
 });
